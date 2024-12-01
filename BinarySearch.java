e23f58830ebb08011e18a268c56009b373b6ceff
Chance03-git <Chancefoster05@gmail.com>
2024-11-30 22:14:47 -0500

    Binary search function defined

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,5 +1,31 @@
 public class BinarySearch {
-// Test cases for the binarySearch method
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // Check if target is present at mid
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            // If target is greater, ignore the left half
+            if (arr[mid] < target) {
+                left = mid + 1;
+            }
+            // If target is smaller, ignore the right half
+            else {
+                right = mid - 1;
+            }
+        }
+
+        // Target not found
+        return -1;
+    }
+
+    // Test cases for the binarySearch method
 public static void main(String[] args) {
     int[] sortedArray = {1, 3, 5, 7, 9, 11, 13, 15};
 
